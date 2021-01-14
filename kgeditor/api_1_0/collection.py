addd132a7e8eb640f911fbff4b3f89accfc52a73
Yufeng Lyu <lvyufeng@cqu.edu.cn>
2021-01-14 21:48:59 +0800

    add verify_graph wraps

--- kgeditor/api_1_0/collection.py
+++ kgeditor/api_1_0/collection.py
@@ -4,7 +4,7 @@ and all operations of them are assigned in this file
 """
 from . import api
 from flask import jsonify, g, request, session
-from kgeditor.utils.common import login_required
+from kgeditor.utils.common import login_required, verify_graph
 from kgeditor import db, arango_conn
 from kgeditor.models import Graph
 from kgeditor.utils.response_code import RET
@@ -14,21 +14,10 @@ from pyArango.consts import *
 
 @api.route('list_documents', methods=['POST'])
 @login_required
+@verify_graph
 def list_documents():
-    req_dict = request.get_json()
-    graph_id = req_dict.get('graph_id')
-    # validate permission
-    try:
-        graph = Graph.query.filter_by(id=graph_id, creator_id=g.user_id).first()
-    except Exception as e:
-        logging.error(e)
-        return jsonify(errno=RET.DBERR, errmsg='数据库异常')
-    else:
-        if graph is None:
-            return jsonify(errno=RET.DBERR, errmsg='文档集合不存在或用户无编辑权限')
-    
     documents = []
-    graph_db = arango_conn['graph_{}'.format(graph_id)]
+    graph_db = arango_conn['graph_{}'.format(g.graph_id)]
     
     for name, collection in graph_db.collections.items():
         if collection.type == COLLECTION_DOCUMENT_TYPE and not name.startswith('_'):
@@ -37,18 +26,9 @@ def list_documents():
 
 @api.route('list_edges', methods=['POST'])
 @login_required
+@verify_graph
 def list_edges():
-    req_dict = request.get_json()
-    graph_id = req_dict.get('graph_id')
-    try:
-        graph = Graph.query.filter_by(id=graph_id, creator_id=g.user_id).first()
-    except Exception as e:
-        logging.error(e)
-        return jsonify(errno=RET.DBERR, errmsg='数据库异常')
-    else:
-        if graph is None:
-            return jsonify(errno=RET.DBERR, errmsg='边集合不存在或用户无编辑权限')
-    graph_db = arango_conn['graph_{}'.format(graph_id)]
+    graph_db = arango_conn['graph_{}'.format(g.graph_id)]
     # graph_db.collections
     edges = []
     for name, collection in graph_db.collections.items():
--- kgeditor/utils/common.py
+++ kgeditor/utils/common.py
@@ -1,7 +1,8 @@
-from flask import session, jsonify, g
+from flask import session, jsonify, g, request
 from kgeditor.utils.response_code import RET
 import functools
-
+from kgeditor.models import Graph
+import logging
 # verify login status
 def login_required(view_func):
     @functools.wraps(view_func)
@@ -12,4 +13,23 @@ def login_required(view_func):
             return view_func(*args, **kwargs)
         else:
             return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+    return wrapper
+
+# verify graph
+def verify_graph(view_func):
+    @functools.wraps(view_func)
+    def wrapper(*args, **kwargs):
+        req_dict = request.get_json()
+        graph_id = req_dict.get('graph_id')
+        # validate permission
+        try:
+            graph = Graph.query.filter_by(id=graph_id, creator_id=g.user_id).first()
+        except Exception as e:
+            logging.error(e)
+            return jsonify(errno=RET.DBERR, errmsg='数据库异常')
+        else:
+            if graph is None:
+                return jsonify(errno=RET.DBERR, errmsg='文档集合不存在或用户无编辑权限')
+            g.graph_id = graph_id
+            return view_func(*args, **kwargs)
     return wrapper            
