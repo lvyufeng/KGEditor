4e1e5879f9d3dcc1f7e98d925a8be3ad3bfab532
lvyufeng <lvyufeng@cqu.edu.cn>
2021-02-09 12:23:21 +0800

    add edge operations

--- kgeditor/api_1_0/graph_edge.py
+++ kgeditor/api_1_0/graph_edge.py
@@ -1,33 +1,108 @@
-@api.route('/link', methods=['POST'])
+from . import api
+from flask import jsonify, g, request, session
+from kgeditor.utils.common import login_required, verify_domain, verify_graph
+from kgeditor.utils.data import text2json
+from kgeditor import db, arango_conn
+from kgeditor.models import Graph
+from kgeditor.utils.response_code import RET
+from sqlalchemy.exc import IntegrityError
+import logging
+from pyArango.theExceptions import *
+import json
+
+# list edge collections
+@api.route('/<int:domain_id>/graph/<int:graph_id>/edge', methods=['GET'])
 @login_required
-def link():
-    """连接Node
-    A shorthand for createEdge that takes two documents as input
+@verify_graph
+def list_edge_collections(domain_id, graph_id):
+    """展示关系类型
     
     Args:
-        link_type:
-        from:
-        to:
-        attributes:
+        graph_id: graph to show the vertex collections
     """
-    graph_id = session.get('graph_id')
-    domain_id = session.get('domain_id')
-    if not all([graph_id, domain_id]):
-        return jsonify(errno=RET.SESSIONERR, errmsg="未选择指定的图谱")
-    logging.info(graph_id)
-    logging.info(domain_id)
     domain_db = arango_conn['domain_{}'.format(domain_id)]
     db_graph = domain_db.graphs['graph_{}'.format(graph_id)]
-    req_dict = request.get_json()
-    link_type = req_dict.pop('link_type')
-    doc1 = req_dict.get('from')
-    doc2 = req_dict.get('to')
-    attributes = req_dict.get('attributes')
-    logging.info(req_dict)
-    # logging.info()
+
+    url = f'{db_graph.getURL()}/edge'
+    resp = arango_conn.session.get(url)
+    if resp.status_code != 200:
+        return jsonify(errno=RET.DBERR, errmsg='数据库异常')
+    data = text2json(resp.text)
+    return jsonify(errno=RET.OK, errmsg="OK", data=data['collections'])
+
+# add vertex
+@api.route('/<int:domain_id>/graph/<int:graph_id>/edge/<collection>', methods=['POST'])
+@login_required
+@verify_graph
+def add_edge(domain_id, graph_id, collection):
+    """add edge
+    """
+    domain_db = arango_conn['domain_{}'.format(domain_id)]
+    db_graph = domain_db.graphs['graph_{}'.format(graph_id)]
+    req = request.get_json()
+    _from = req['from']
+    _to = req['to']
+    attribute = req['attribute']
+    if not all([_from, _to]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
     try:
-        data = db_graph.link(link_type, doc1, doc2, attributes)
+        db_graph.createEdge(collection, _from, _to, attribute)
+    except CreationError as e:
+        logging.error(e)
+        return jsonify(errno=RET.DATAEXIST, errmsg="添加边失败")
     except Exception as e:
         logging.error(e)
-        return jsonify(errno=RET.DATAERR, errmsg="添加连接失败")
-    return jsonify(errno=RET.OK, errmsg="OK")
+        return jsonify(errno=RET.DBERR, errmsg="添加边失败")
+    return jsonify(errno=RET.OK, errmsg="添加边成功")
+# get edge
+@api.route('/<int:domain_id>/graph/<int:graph_id>/edge/<collection>/<edge>', methods=['GET'])
+@login_required
+def get_edge(domain_id, graph_id, collection, edge):
+    domain_db = arango_conn['domain_{}'.format(domain_id)]
+    db_graph = domain_db.graphs['graph_{}'.format(graph_id)]
+    url = "%s/edge/%s/%s" % (db_graph.getURL(), collection, edge)
+    try:
+        r = db_graph.connection.session.get(url)        
+    except Exception as e:
+        logging.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询失败")
+    if r.status_code == 200:
+        data = r.json()
+        return jsonify(errno=RET.OK, errmsg="查询成功", data=data['edge'])
+    return jsonify(errno=RET.DBERR, errmsg="查询失败")
+
+# delete edge
+@api.route('/<int:domain_id>/graph/<int:graph_id>/edge/<collection>/<edge>', methods=['DELETE'])
+@login_required
+def delete_edge(domain_id, graph_id, collection, edge):
+    # pass
+    domain_db = arango_conn['domain_{}'.format(domain_id)]
+    db_graph = domain_db.graphs['graph_{}'.format(graph_id)]
+    
+    try:
+        document = domain_db[collection][edge]
+        db_graph.deleteEdge(document)
+    except Exception as e:
+        logging.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="删除失败")
+    return jsonify(errno=RET.OK, errmsg="删除成功")
+
+# update edge
+@api.route('/<int:domain_id>/graph/<int:graph_id>/edge/<collection>/<edge>', methods=['PATCH'])
+@login_required
+def update_edge(domain_id, graph_id, collection, edge):
+    # pass
+    domain_db = arango_conn['domain_{}'.format(domain_id)]
+    db_graph = domain_db.graphs['graph_{}'.format(graph_id)]
+    url = "%s/vertex/%s/%s" % (db_graph.getURL(), collection, edge)
+    req = request.get_json()
+    if not req:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+    try:
+        r = db_graph.connection.session.patch(url, data = json.dumps(req, default=str))        
+    except Exception as e:
+        logging.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="修改失败")
+    if r.status_code == 200 or r.status_code == 202:
+        return jsonify(errno=RET.OK, errmsg="修改成功")
+    return jsonify(errno=RET.DBERR, errmsg="修改失败")
