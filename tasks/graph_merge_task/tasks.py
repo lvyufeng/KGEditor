0b54079c0f5fcf9edecae6dfd6b68bf1189bee8a
Lv Yufeng <lvyufeng@LvdeMacBook-Pro.local>
2021-05-04 13:47:19 +0800

    merge

---
+++ tasks/graph_merge_task/tasks.py
@@ -0,0 +1,9 @@
+from tasks.main import celery_app as app
+
+@app.task
+def open_file(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        print(f.readline())
+    f.close()
+    
+    return 1
