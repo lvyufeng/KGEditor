2b80e16f6c1ac2aba0abf838d3f191fad43983d7
lvyufeng <lvyufeng@cqu.edu.cn>
2021-05-18 10:43:46 +0800

    upgrade upload api

--- kgeditor/api_1_0/upload.py
+++ kgeditor/api_1_0/upload.py
@@ -1,22 +1,35 @@
-from flask_restx import Resource, fields
-from . import api
-from kgeditor.dao.project import ProjectDAO
-from flask import abort, session, request
-import re
 import logging
+from . import api
+from flask import abort
+from flask_restx import Resource
+from kgeditor import data, text
 from kgeditor.utils.common import login_required
-from kgeditor.constants import TASK_ANNOTATION, TASK_FUSION, TASK_GRAPH
 from werkzeug.datastructures import FileStorage
+
+dtype_dict = {
+    'text': text,
+    'data': data
+}
 
 ns = api.namespace('Upload', path='/', description='Upload operations')
 upload_parser = ns.parser()
 upload_parser.add_argument('file', location='files',
                            type=FileStorage, required=True)
-@ns.route('/upload/')
+
+@ns.route('/upload/<string:dtype>')
 @ns.expect(upload_parser)
 class Upload(Resource):
-    def post(self):
+    """Upload a file like txt or csv"""
+    @ns.doc('upload_file')
+    @login_required
+    def post(self, dtype):
+        """Upload file to server"""
         args = upload_parser.parse_args()
         uploaded_file = args['file']  # This is FileStorage instance
-        url = do_something_with_file(uploaded_file)
-        return {'url': url}, 201
+        try:
+            file_name = dtype_dict.get(dtype).save(uploaded_file)
+            logging.info(file_name)
+        except Exception as e:
+            logging.error(e)
+            return abort(500, 'Upload file failed.')
+        return {'message': 'Upload file succeed.', 'url': file_name}, 201
